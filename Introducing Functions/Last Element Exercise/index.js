8b294d4bebdb446024805307c680dd3e00d58f01
Oscar López <machochinnio@hotmail.com>
2020-11-16 17:37:59 -0600

    Last Element Of Array Exercise

---
+++ Introducing Functions/Last Element Exercise/index.js
@@ -0,0 +1,11 @@
+// DEFINE YOUR FUNCTION BELOW:
+function lastElement (arr)
+{
+    let result = null;
+
+    if (arr.length) {
+        result = arr[arr.length - 1];
+    }
+
+    return result;
+}
