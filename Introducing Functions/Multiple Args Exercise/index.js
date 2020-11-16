4440fa268bde5c65c9842ac467b8c097c5389d54
Oscar López <machochinnio@hotmail.com>
2020-11-16 17:28:39 -0600

    Snake Eyes Exercise

---
+++ Introducing Functions/Multiple Args Exercise/index.js
@@ -0,0 +1,9 @@
+// define isSnakeEyes below:
+function isSnakeEyes(dice1, dice2)
+{
+    if(dice1 === 1 && dice2 === 1) {
+        console.log("Snake Eyes!");
+    } else {
+        console.log("Not Snake Eyes!");
+    }
+}
