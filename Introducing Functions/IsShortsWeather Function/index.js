e05d82ce6afe03daa4d10b294729c1348a05dd3a
Oscar López <machochinnio@hotmail.com>
2020-11-16 17:35:01 -0600

    Is Shorts Weather Function

---
+++ Introducing Functions/IsShortsWeather Function/index.js
@@ -0,0 +1,11 @@
+// DEFINE YOUR FUNCTION BELOW:
+function isShortsWeather (temperature)
+{
+    let result = false;
+
+    if(temperature >= 75) {
+        result = true;
+    }
+
+    return result;
+}
