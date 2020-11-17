217b3abd2e9f1c17f9ec2bea83f9807f09e5ad05
Oscar López <machochinnio@hotmail.com>
2020-11-17 14:57:44 -0600

    addEventListener

---
+++ The Missing Piece DOM Events/Click Events Exercise/app.js
@@ -0,0 +1,5 @@
+document.getElementById("hello")
+        .addEventListener("click", () => console.log("Hello"));
+
+document.getElementById("goodbye")
+    .addEventListener("click", () => console.log("goodbye"));
