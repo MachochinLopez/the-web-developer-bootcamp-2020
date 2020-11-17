6555c725c6d258850e7a5a54f35e40ef276968d0
Oscar López <machochinnio@hotmail.com>
2020-11-17 14:24:05 -0600

    100 Buttons

---
+++ The World Of The DOM/100 Button Insanity Exercise/app.js
@@ -0,0 +1,6 @@
+// WRITE YOUR CODE IN HERE:
+for (let i = 0; i < 100; i++) {
+    let button  = document.createElement('button');
+    button.innerText = "Hey!";
+    document.getElementById("container").appendChild(button);
+}
