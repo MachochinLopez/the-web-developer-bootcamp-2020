525edfc35e493ba97a0b9a97c2a5d8129c939bdd
Oscar López <machochinnio@hotmail.com>
2020-11-24 12:56:06 -0600

    Bootstrap y Partials con EJS.

--- Creating Dynamic HTML With Templating/Bootstrap Demo/index.js
+++ Creating Dynamic HTML With Templating/Bootstrap Demo/index.js
@@ -16,11 +16,13 @@ app.get("/cats", (req, res) => {
     const cats = [
         "Blue", "Rocket", "Monty", "Carlitos", "Juan"
     ];
+
     res.render("cats", { cats });
 });
 
 app.get("/rand", (req, res) => {
     const num = Math.floor(Math.random() * 10) + 1;
+
     res.render("random", { rand: num });
 });
 
