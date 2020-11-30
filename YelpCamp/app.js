3c567c262527471e4159e4c9c605e4af6e3bea46
Oscar López <machochinnio@hotmail.com>
2020-11-29 22:04:11 -0600

    YelpCamp Campgrounds CRUD

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const path = require('path');
 
@@ -11,6 +12,9 @@ db.once('open', () => {
 });
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -26,6 +30,45 @@ const Campground = require('./models/campground');
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds });
+});
+
+// CAMPGROUND CREATE
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/create');
+});
+
+// CAMPGROUND STORE
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
+// CAMPGROUND SHOW
+app.get('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/show', { campground });
+});
+
+// CAMPGROUND EDIT
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/edit', { campground });
+});
+
+// CAMPGROUND UPDATE
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
 });
 
 app.listen(3000, () => {
