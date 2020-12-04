04135a1a7cf1c6f5e8c70a5cd4d310f14bdc8871
Oscar López <machochinnio@hotmail.com>
2020-12-03 22:46:22 -0600

    AuthDemo

---
+++ 50 - BcryptDemo/index.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const User = require('./models/user');
+const path = require('path');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+const { nextTick } = require('process');
+
+mongoose.connect('mongodb://localhost:27017/authDemo', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, "connection error:"));
+db.once('open', () => {
+    console.log('Database connected');
+});
+
+// MIDDLEWARE
+const requireLogin = (req, res, next) => {
+    if (!req.session.user_id) {
+        return res.redirect('/login');
+    }
+    next();
+};
+
+app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.send('hola');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const foundUser = await User.findAndValidate(username, password);
+    if (foundUser) {
+        req.session.user_id = foundUser._id;
+        res.redirect('/');
+    } else {
+        res.redirect('/login');
+    }
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    const user = new User({ username, password });
+    await user.save();
+    req.session.user_id = user._id;
+    res.redirect('/');
+});
+
+app.get('/secret', requireLogin, (req, res) => {
+    res.render('secret');
+});
+
+app.get('/topsecret', requireLogin, (req, res) => {
+    res.send('TOP SECRET!');
+});
+
+app.post('/logout', (req, res) => {
+    req.session.user_id = null;
+    res.redirect('/login');
+});
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
+});
